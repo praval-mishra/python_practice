8cca7c9357e220b50ee924f248f2b97cfaeab109
praval-mishra <pravalmishra10@gmail.com>
2026-03-09 23:06:00 +0530

    small change

--- Inheritance.py
+++ Inheritance.py
@@ -12,7 +12,21 @@ class Employee:
         pay=int(self.payself.raise_amt)
         
 class Developers(Employee):
-    pass
-emp1=Developers('raghu','kumar',600000)
-emp2=Developers('ganesh','kumar',900000)
+    raise_amt=1.10
+    def __init__(self, first, last, pay,progLang):
+        super().__init__(first, last, pay)
+        self.progLang=progLang
+        
+emp1=Developers('raghu','kumar',600000,'python')
+emp2=Developers('ganesh','kumar',900000,'Java')
 print(emp1.FullName())
+#The help() method is a built-in Python function
+#  used to access documentation for modules, classes,
+#  methods, and variables directly from your terminal
+#  or script. It is like having a built-in manual for
+#  the entire Python language.
+# print(help(Developers))
+print(emp2.progLang)
+
+class manager(Employee):
+    pass
